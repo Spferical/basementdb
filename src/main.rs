0db8a1a6f327b7208df5194902e7e7a7e50019b9
Matthew Pfeiffer <spferical@gmail.com>
2018-04-06 19:54:28 -0400

    Tarpc hello world

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,37 @@
+#![feature(plugin, use_extern_macros)]
+#![plugin(tarpc_plugins)]
+
+#[macro_use]
+extern crate tarpc;
+
+use tarpc::sync::{client, server};
+use tarpc::sync::client::ClientExt;
+use tarpc::util::Never;
+use std::sync::mpsc;
+use std::thread;
+
+service! {
+    rpc hello(name: String) -> String;
+}
+
+#[derive(Clone)]
+struct HelloServer;
+
+impl SyncService for HelloServer {
+    fn hello(&self, name: String) -> Result<String, Never> {
+        Ok(format!("Hello, {}!", name))
+    }
+}
+
 fn main() {
-    println!("Hello, world!");
+    let (tx, rx) = mpsc::channel();
+    thread::spawn(move || {
+        let handle = HelloServer.listen("localhost:10000",
+            server::Options::default()).unwrap();
+        tx.send(handle.addr()).unwrap();
+        handle.run();
+    });
+    let addr = rx.recv().unwrap();
+    let client = SyncClient::connect(addr, client::Options::default()).unwrap();
+    println!("{}", client.hello("world".to_string()).unwrap());
 }
