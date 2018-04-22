63cbbb1fb1df08a727381836c2568b02b592b64e
Srinivas Kaza <srinivas@kaza.io>
2018-04-22 15:25:03 -0400

    MessageType enum

--- src/message.rs
+++ src/message.rs
@@ -1,5 +1,34 @@
-struct Message {
-    v: usize,        // Current view number
-    n: usize,        // Highest sequence number executed
+use std::vec::Vec;
 
+type DigestHash = [u64; 4];
+type HashChain = Vec<DigestHash>;
+
+enum MessageType {
+    // 4.4 Sequence Number Assignment
+    Request,   // Client Request (Request)
+    OR,        // OrderedRequest (Request)
+    SpecReply, // SpecReply (Reply, in response to `Request` from client)
+    Commit,    // Commit (Request)
+    Reply,     // Reply (Reply, in response to client request)
+    FillHole,  // Fill Hole (Reply, in response to `Commit` or `OR` from server)
+
+    // 4.5 View Change Protocol
+    IHateThePrimary, // IHateThePrimary (Request)
+    ViewChange,      // ViewChange (Request)
+    NewView,         // NewView (Request)
+    ViewConfirm,     // ViewConfirm (Request)
+
+    // 4.6-4.7 Detecting and Merging Concurrent Histories
+    POMMsg, // Proof of Misbehavior (Request)
+    PODMsg, // Proof of Divergence (Request)
+    POAMsg, // Proof of Absence (Request)
+
+    // 4.8 Garbage Collection
+    CheckPoint, // CheckPoint (Request)
+}
+
+struct Message {
+    v: usize, // Current view number
+    n: usize, // Highest sequence number executed
+    h: usize,
 }
