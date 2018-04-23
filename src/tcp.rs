3683acfdfa6d8625b95fa6ff051add1af082464c
Srinivas Kaza <srinivas@kaza.io>
2018-04-23 01:47:03 -0400

    Refactors constructors out and adds callbacks

--- src/tcp.rs
+++ src/tcp.rs
@@ -7,93 +7,118 @@ use std::time::Duration;
 use message::Message;
 
 const MAX_BUF_SIZE: usize = 1048576;
-const READ_TIMEOUT: Duration = Duration::new(30, 0);
-const WRITE_TIMEOUT: Duration = Duration::new(30, 0);
-const CONNECT_TIMEOUT: Duration = Duration::new(30, 0);
+const READ_TIMEOUT: u64 = 30;
+const WRITE_TIMEOUT: u64 = 30;
+const CONNECT_TIMEOUT: u64 = 30;
 
 #[derive(Clone)]
-struct TCPServer {
+pub struct TCPServer {
     ip_and_port: String,
+    callback: fn(Message),
 }
 
-impl TCPServer {
-    fn start_server(self) {
-        let addr: SocketAddr = self.ip_and_port.parse().unwrap();
-        let listener = TcpListener::bind(addr).unwrap();
+pub fn read_string_from_socket(mut sock: &TcpStream) -> Option<String> {
+    let mut buf: [u8; 256] = [0; 256];
+    let mut curr_buf: Vec<u8> = Vec::new();
 
-        for stream_result in listener.incoming() {
-            let new_self = self.clone();
-            match stream_result {
-                Err(err) => println!("Err {:?}", err),
-                Ok(stream) => {
-                    thread::spawn(move || new_self.handle_client(stream));
-                    return;
+    'outer: loop {
+        match sock.read(&mut buf) {
+            Err(err) => println!("Unable to read from TCP stream: {:?}", err),
+            Ok(num_bytes) => {
+                for i in 0..num_bytes {
+                    let c = buf[i];
+                    curr_buf.push(c);
+                    if c == 0 {
+                        break 'outer;
+                    }
                 }
-            };
+
+                if num_bytes != 256 {
+                    break 'outer;
+                }
+            }
+        }
+
+        if curr_buf.len() > MAX_BUF_SIZE {
+            println!("Message too long!");
+            return None;
         }
     }
 
-    fn handle_client(&self, mut client: TcpStream) {
-        client.set_read_timeout(Some(READ_TIMEOUT));
-        let mut buf: [u8; 256] = [0; 256];
-        let mut curr_buf: Vec<u8> = Vec::new();
-        loop {
-            match client.read(&mut buf) {
-                Err(err) => println!("Unable to read from TCP stream: {:?}", err),
-                Ok(num_bytes) => {
-                    for i in 0..num_bytes {
-                        let c = buf[i];
-                        curr_buf.push(c);
-                        if c == 0 {
-                            break;
-                        }
-                    }
-
-                    if num_bytes != 256 {
-                        break;
-                    }
-                }
-            }
-
-            if curr_buf.len() > MAX_BUF_SIZE {
-                println!("Message too long!");
-                return;
-            }
+    match str::from_utf8(&curr_buf) {
+        Err(e) => {
+            println!("Invalid string read from socket: {:?}!", e);
+            return None;
         }
+        Ok(v) => {
+            return Some(v.to_string());
+        }
+    };
+}
 
-        let s: String;
-        match str::from_utf8(&curr_buf) {
-            Err(e) => {
-                println!("Invalid string read from socket: {:?}!", e);
+pub fn start_server(ip_and_port: String, callback: fn(Message)) {
+    let addr: SocketAddr = ip_and_port.parse().unwrap();
+    let listener = TcpListener::bind(addr).unwrap();
+
+    let server = TCPServer {
+        ip_and_port: ip_and_port,
+        callback: callback,
+    };
+
+    for stream_result in listener.incoming() {
+        let new_self = server.clone();
+        match stream_result {
+            Err(err) => println!("Err {:?}", err),
+            Ok(stream) => {
+                thread::spawn(move || new_self.handle_client(stream));
                 return;
-            }
-            Ok(v) => {
-                s = v.to_string();
             }
         };
     }
 }
 
-struct TCPClient {
+impl TCPServer {
+    fn handle_client(&self, mut client: TcpStream) {
+        client.set_read_timeout(Some(Duration::new(READ_TIMEOUT, 0)));
+
+        loop {
+            let s: String;
+            match read_string_from_socket(&client) {
+                None => {
+                    return;
+                }
+                Some(v) => s = v,
+            };
+
+            // Deserialize and call callback
+        }
+    }
+}
+
+pub struct TCPClient {
     ip_and_port: String,
     stream: TcpStream,
-    callback: Fn(Message),
+    callback: fn(Message),
+}
+
+pub fn connect_to_server(ip_and_port: String, callback: fn(Message)) -> Option<TCPClient> {
+    let sock_addr: SocketAddr = ip_and_port.parse().unwrap();
+    if let Ok(stream) = TcpStream::connect_timeout(&sock_addr, Duration::new(CONNECT_TIMEOUT, 0)) {
+        return Some(TCPClient {
+            ip_and_port: ip_and_port,
+            stream: stream,
+            callback: callback,
+        });
+    } else {
+        println!("Failed to connect!");
+        return None;
+    }
 }
 
 impl TCPClient {
-    fn connect_to_server(&self) -> bool {
-        let sock_addr: SocketAddr = self.ip_and_port.parse().unwrap();
-        if let Ok(stream) = TcpStream::connect_timeout(&sock_addr, CONNECT_TIMEOUT) {
-            self.stream = stream;
-            return true;
-        } else {
-            println!("Failed to connect!");
-            return false;
-        }
-    }
-
-    fn send_obj(&self, m: String) -> bool {
-        self.stream.set_write_timeout(Some(WRITE_TIMEOUT));
+    pub fn send_obj(&mut self, m: String) -> bool {
+        self.stream
+            .set_write_timeout(Some(Duration::new(WRITE_TIMEOUT, 0)));
         match self.stream.write(m.as_bytes()) {
             Err(err) => {
                 println!("Unable to write to TCP stream: {:?}", err);
