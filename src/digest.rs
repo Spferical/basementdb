9ef1f22fd1a08b2fbcdcefcaf2b622f6ffc32330
Matthew Pfeiffer <spferical@gmail.com>
2018-06-16 21:45:18 -0700

    simplify u8 to u64 conv

--- src/digest.rs
+++ src/digest.rs
@@ -8,14 +8,11 @@ pub type HashDigest = [u64; 4];
 /// The hash chain
 pub type HashChain = Vec<HashDigest>;
 
-fn u8_conv(a: u8) -> u64 {
-    (u64::from(a)) & 0xff
-}
-
 fn u8to64(p: [u8; 64], i: usize) -> u64 {
-    u8_conv(p[i]) | u8_conv(p[i + 1]) << 8 | u8_conv(p[i + 2]) << 16
-        | u8_conv(p[i + 3]) << 24 | u8_conv(p[i + 4]) << 32 | u8_conv(p[i + 5]) << 40
-        | u8_conv(p[i + 6]) << 48 | u8_conv(p[i + 7]) << 56
+    u64::from(p[i]) | u64::from(p[i + 1]) << 8
+        | u64::from(p[i + 2]) << 16 | u64::from(p[i + 3]) << 24
+        | u64::from(p[i + 4]) << 32 | u64::from(p[i + 5]) << 40
+        | u64::from(p[i + 6]) << 48 | u64::from(p[i + 7]) << 56
 }
 
 fn conv(digest: [u8; 64]) -> HashDigest {
