873f58ba2f16e34f813e137b71c96432062da5b0
Matthew Pfeiffer <spferical@gmail.com>
2019-01-01 23:58:52 -0500

    Load cluster config file

--- src/main.rs
+++ src/main.rs
@@ -1,26 +1,49 @@
 extern crate basementdb;
-extern crate config;
 
-use std::env;
+#[macro_use]
+extern crate serde_derive;
+#[macro_use]
+extern crate clap;
+extern crate serde_json;
+
+use clap::{App, Arg, SubCommand};
+use std::fs::File;
+
+#[derive(Serialize, Deserialize, Debug)]
+struct ClusterConfig {
+    nodes: Vec<String>,
+    f: u64,
+}
+
+fn load_cluster_config(path: &str) -> ClusterConfig {
+    let file = File::open(path).expect("file not found");
+    let config: ClusterConfig = serde_json::from_reader(file).expect("error while reading json");
+    config
+}
 
 fn main() {
-    let args: Vec<_> = env::args().collect();
-    if args.len() != 2 {
-        println!("USAGE: {} URL", args[0]);
-        return;
-    }
-    let _url = args[1].clone();
+    let matches = App::new("BasementDB")
+        .version("0.1")
+        .author(crate_authors!())
+        .subcommand(
+            SubCommand::with_name("gen_keys")
+                .about("Generates a public/private key pair")
+                .arg(Arg::with_name("name").required(true)),
+        )
+        .arg(Arg::with_name("bind url").required(true).takes_value(true))
+        .arg(
+            Arg::with_name("private key path")
+                .required(true)
+                .takes_value(true),
+        )
+        .arg(
+            Arg::with_name("cluster config file")
+                .required(true)
+                .takes_value(true),
+        )
+        .get_matches();
 
-    let mut settings = config::Config::default();
-    settings.merge(config::File::with_name("config")).unwrap();
-    settings
-        .merge(config::Environment::with_prefix("BDB"))
-        .unwrap();
-    let debug = settings.get_bool("debug").unwrap();
-    println!("debug: {:?}", debug);
-    let servers = settings.get::<Vec<String>>("servers").unwrap();
-    println!("servers: {:?}", servers);
-
-    // TODO: read private and public key from path in config
-    // and start the server
+    let cluster_config_path = matches.value_of("cluster config file").unwrap();
+    let cluster_config = load_cluster_config(cluster_config_path);
+    println!("{:?}", cluster_config);
 }
